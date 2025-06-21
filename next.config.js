8e1e4b49404260cf9e6b5e3fdf9854ee9c66dd87
Youness <y.ouzzedi@hotmail.com>
2025-06-21 23:35:44 +0100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['coin-images.coingecko.com'], // <-- autorise ce domaine
+  },
+}
 
 module.exports = nextConfig
